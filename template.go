4057b3432aaa8408ccf43af3c9855ce095f71caa
Matt Gleich <git@mattglei.ch>
2025-01-08 01:25:09 -0500

    feat: change html template
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- template.go
+++ template.go
@@ -11,13 +11,23 @@ var htmlTemplate = template.Must(template.New("gometa").Parse(`
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
     <meta
       name="go-import"
-      content="pkg.mattglei.ch/{{.ProjectName}} git https://github.com/gleich/{{.ProjectRoot}}.git"
+      content="pkg.mattglei.ch/{{.ProjectName}} git https://github.com/gleich/{{.ProjectRoot}}"
+    />
+    <meta
+      name="go-source"
+      content="pkg.mattglei.ch/{{.ProjectName}} https://github.com/gleich/{{.ProjectRoot}} https://github.com/gleich/tree/main/{{.ProjectRoot}}{/dir} https://github.com/gleich/{{.ProjectRoot}}/blob/main{/dir}/{file}#{line}"
     />
     <meta http-equiv="refresh" content="0; url=https://pkg.go.dev/{{.ProjectName}}">
     <title>{{.ProjectName}}</title>
   </head>
+
+   <body>
+    <a href="https://github.com/gleich/{{.ProjectRoot}}">github.com/gleich/{{.ProjectRoot}}</a><br>
+    <a href="https://github.com/gleich/vanityprox" target="_blank">Proxied by gleich/vanityprox</a>
+  </body>
 </html>
 `))
