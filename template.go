7ef1f98ec0738cbf35a12be5e44174d61fbe8be9
Matt Gleich <git@mattglei.ch>
2025-01-17 10:37:47 -0500

    feat: change refresh url
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- template.go
+++ template.go
@@ -20,7 +20,7 @@ var htmlTemplate = template.Must(template.New("gometa").Parse(`<!DOCTYPE html>
       name="go-source"
       content="pkg.mattglei.ch/{{.ProjectName}} https://github.com/gleich/{{.ProjectRoot}} https://github.com/gleich/{{.ProjectRoot}}/tree/main/{/dir} https://github.com/gleich/{{.ProjectRoot}}/blob/main{/dir}/{file}#L{line}"
     />
-    <meta http-equiv="refresh" content="0; url=https://pkg.go.dev/github.com/gleich/{{.ProjectName}}">
+    <meta http-equiv="refresh" content="0; url=https://pkg.go.dev/pkg.mattglei.ch/{{.ProjectName}}">
     <link rel="icon" href="https://mattglei.ch/favicon.ico">
     <title>{{.ProjectName}}</title>
   </head>
