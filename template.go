b4d82ace33efd88c5052fd0b33d019db32dadd5c
Matt Gleich <git@mattglei.ch>
2025-01-27 13:42:13 -0500

    feat: embed html file directly
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- template.go
+++ template.go
@@ -1,31 +1,16 @@
 package main
 
-import "html/template"
+import (
+	_ "embed"
+	"html/template"
+)
 
 type templateData struct {
 	ProjectName string
 	ProjectRoot string
 }
 
-var htmlTemplate = template.Must(template.New("gometa").Parse(`<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width" />
-    <meta
-      name="go-import"
-      content="pkg.mattglei.ch/{{.ProjectName}} git https://github.com/gleich/{{.ProjectRoot}}"
-    />
-    <meta
-      name="go-source"
-      content="pkg.mattglei.ch/{{.ProjectName}} https://github.com/gleich/{{.ProjectRoot}} https://github.com/gleich/{{.ProjectRoot}}/tree/main/{/dir} https://github.com/gleich/{{.ProjectRoot}}/blob/main{/dir}/{file}#L{line}"
-    />
-    <meta http-equiv="refresh" content="0; url=https://pkg.go.dev/pkg.mattglei.ch/{{.ProjectName}}">
-    <link rel="icon" href="https://mattglei.ch/favicon.ico">
-    <title>{{.ProjectName}}</title>
-  </head>
-   <body>
-    <a href="https://github.com/gleich/{{.ProjectRoot}}">github.com/gleich/{{.ProjectRoot}}</a><br>
-    <a href="https://github.com/gleich/vanityprox" target="_blank">Proxied by gleich/vanityprox</a>
-  </body>
-</html>`))
+//go:embed template.html
+var html string
+
+var htmlTemplate = template.Must(template.New("gometa").Parse(html))
