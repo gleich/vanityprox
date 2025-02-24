9a714b6bfab3f59ed90faa4aa4f54f0101b89f47
Matt Gleich <git@mattglei.ch>
2025-02-24 12:10:02 -0500

    feat: log out response status codes
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- main.go
+++ main.go
@@ -83,10 +83,26 @@ func handle(conf config) http.HandlerFunc {
 	}
 }
 
+type wrappedWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (w *wrappedWriter) WriteHeader(code int) {
+	w.ResponseWriter.WriteHeader(code)
+	w.statusCode = code
+}
+
 func logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		next.ServeHTTP(w, r)
-		timber.Done(r.URL.Path, time.Since(start))
+
+		wrapped := &wrappedWriter{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(wrapped, r)
+		timber.Done(wrapped.statusCode, r.URL.Path, time.Since(start))
 	})
 }
